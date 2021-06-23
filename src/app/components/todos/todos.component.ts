3a59b3c51fe3920b0496aa07a627f73a6ccbfefa
Fidel Hdz <fidel.hdz@me.com>
2021-06-23 12:05:13 -0500

    Giving some styles to todo list

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -3,50 +3,49 @@ import { Todo } from './../../models/Todo';
 
 
 @Component({
-  selector: 'app-todos',
-  templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.css']
+    selector: 'app-todos',
+    templateUrl: './todos.component.html',
+    styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
 
-  todos:Todo[] = [];
-  inputTodo:string = '';
+    todos:Todo[] = [];
+    inputTodo:string = '';
 
-  constructor() {
-    
-  }
+    constructor() {
 
-  ngOnInit(): void {
-    this.todos = [
-      {
-        content: 'First todo',
-        completed: false
-      },
-      {
-        content: 'Second todo',
-        completed: true
-      }
-    ];
-  }
+    }
 
-  toggleDone (id:number) {
-      this.todos.map( (v, i) => {
-        
-        if ( i == id ) { v.completed = !v.completed; }
-        console.log(v);
-        return v;
-      } );
-  }
+    ngOnInit(): void {
+        this.todos = [
+            {
+                content: 'First todo',
+                completed: false
+            },
+            {
+                content: 'Second todo',
+                completed: true
+            }
+        ];
+    }
 
-  deleteTodo (id:number) {
-    this.todos = this.todos.filter((v, i) =>  i !== id);
-  }
+    toggleDone (id:number) {
+        this.todos.map( (v, i) => {
+            if ( i == id ) { v.completed = !v.completed; }
+            console.log(v);
+            return v;
+        });
+    }
 
-  addTodo () {
-    this.todos.push({
-      content: this.inputTodo,
-      completed: false
-    });
-    this.inputTodo = '';
-  }
+    deleteTodo (id:number) {
+        this.todos = this.todos.filter((v, i) =>  i !== id);
+    }
+
+    addTodo () {
+        this.todos.push({
+            content: this.inputTodo,
+            completed: false
+        });
+        this.inputTodo = '';
+    }
 }
